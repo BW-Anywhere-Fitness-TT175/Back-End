37b93b0e39aaf43a19590f44a72baec00f8dc6d6
ChadDiaz <chadcdiaz@gmail.com>
2021-04-22 19:31:01 -0400

    slight change to row names - became more consistent with naming

--- data/migrations/20210422023517_create_tables.js
+++ data/migrations/20210422023517_create_tables.js
@@ -36,14 +36,14 @@ exports.up = function (knex) {
     })
     .createTable("classes", (tbl) => {
       tbl.increments();
-      tbl.text("className", 20).notNullable().unique();
-      tbl.text("startTime").notNullable();
+      tbl.text("class_name", 20).notNullable().unique();
+      tbl.text("start_time").notNullable();
       tbl.integer("class_date").notNullable();
       tbl.decimal("duration").notNullable();
-      tbl.integer("intensityLevel").notNullable();
+      tbl.integer("intensity_level").notNullable();
       tbl.text("location", 20).notNullable();
-      tbl.integer("registeredClients"); // NOTE Once registeredClients is equal to maxClassSize registration closes
-      tbl.integer("maxClassSize").notNullable();
+      tbl.integer("registered_students"); // NOTE Once registeredClients is equal to maxClassSize registration closes
+      tbl.integer("max_class_size").notNullable();
       tbl
         .integer("instructor_id")
         .unsigned()
