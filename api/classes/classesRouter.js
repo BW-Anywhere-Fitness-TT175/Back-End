4123152d534b2c907eb353bb5cbe47f4bc5e5cef
ChadDiaz <chadcdiaz@gmail.com>
2021-04-25 18:13:18 -0400

    working on classes and users

--- api/classes/classesRouter.js
+++ api/classes/classesRouter.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Classes = require("./classesModel.js");
 
+// NOTE get a list of all classes - not restricted
 router.get("/", async (req, res, next) => {
   try {
     const classes = await Classes.getClasses();
@@ -10,5 +11,30 @@ router.get("/", async (req, res, next) => {
     next();
   }
 });
+
+// NOTE get a specific class - not restricted
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const oneClass = await Classes.getClassById(id);
+    res.status(200).json(oneClass);
+  } catch (err) {
+    err.message = "Server failed to get class";
+    next(err);
+  }
+});
+
+// NOTE get a list of all users per class - not restricted
+// router.get("/:classId/users", async (req, res, next) => {
+//   const { classId } = req.params;
+//   try {
+//     const list = await Classes.getUsersByClass(classId);
+//     res.status(200).json(list);
+//   } catch (err) {
+//     err.message = "Server failed to get class with registered students";
+//     console.log(err);
+//     next(err);
+//   }
+// });
 
 module.exports = router;
--- api/users/usersModel.js
+++ api/users/usersModel.js
@@ -16,10 +16,14 @@ function getFullUserDetails(id) {
   return db("users").first().where({ "users.id": id });
 }
 
-function getUserByEmail(email) {
-  return db("users").first().where({ email });
+// NOTE to be used in the AUTH router
+// POST /api/auth/login
+function findBy(filter) {
+  return db("users").first().where({ filter });
 }
 
+// NOTE to be used in the AUTH router
+// POST /api/auth/register
 async function addUser(newUser) {
   const [newId] = await db("users").insert(newUser, ["id"]);
   return getUsersById(newId.id ?? newId);
@@ -35,7 +39,28 @@ async function addClass(newClass) {}
 module.exports = {
   getUsersById,
   getFullUserDetails,
-  getUserByEmail,
+  findBy,
   addUser,
   editUser,
 };
+
+
+
+
+function getClassByUserId(userId) {
+  return db("user_classes")
+    .join("users", "users.id", "=", "user_classes.user_id")
+    .join("classes", "classes.id", "=", "user_classes.class_id")
+    .select(
+      "users.id AS user_id",
+      "classes.id as class_id",
+      "users.username",
+      "classes.start_time",
+      "classes.class_date",
+      "classes.duration",
+      "classes.type",
+      "classes.intensity",
+      "classes.location"
+    )
+    .where({ "users.id": userId });
+}
