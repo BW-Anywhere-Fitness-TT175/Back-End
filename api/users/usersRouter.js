c77daf342f939990d4350f0deb852d88de2a2d00
ChadDiaz <chadcdiaz@gmail.com>
2021-04-25 19:07:35 -0400

    worked with Sarah on usersModel to see if working

--- api/users/usersRouter.js
+++ api/users/usersRouter.js
@@ -17,4 +17,33 @@ router.get("/:id", mw.checkUserId, (req, res, next) => {
   }
 });
 
+router.put("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const changes = req.body;
+  try {
+    const changedUser = await User.editUser(id, changes);
+    if (changedUser === 1) {
+      res.status(202).json({ message: "User was successfully updated" });
+    } else {
+      res.status(404).json({ message: "User was not updated" });
+    }
+  } catch (err) {
+    err.message = "server failed to edit the user";
+    next(err);
+  }
+});
+
+
+
+// router.post("/", async (req, res, next) => {
+//   const creds = req.body;
+//   try {
+//     const newUser = await User.addUser(creds);
+//     res.status(201).json(newUser);
+//   } catch (err) {
+//     err.message = "server failed to add new user";
+//     next(err);
+//   }
+// });
+
 module.exports = router;
