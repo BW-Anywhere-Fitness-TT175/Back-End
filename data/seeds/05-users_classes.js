e6dd813fe7c31e442c93967ebe916c95621e05ed
ChadDiaz <chadcdiaz@gmail.com>
2021-04-26 20:49:04 -0400

    added an additionl seed to the users_classes table

--- data/seeds/05-users_classes.js
+++ data/seeds/05-users_classes.js
@@ -5,6 +5,7 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex("users_classes").insert([
+        { user_id: 1, class_id: 1 },
         { user_id: 3, class_id: 1 },
         { user_id: 2, class_id: 2 },
         { user_id: 1, class_id: 3 },
