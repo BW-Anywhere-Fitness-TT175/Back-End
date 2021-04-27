8e31198fd0bb822882470f3375f7e66e735b17a8
ChadDiaz <chadcdiaz@gmail.com>
2021-04-26 21:48:43 -0400

    moved the auth endpoints to the usersRouter

--- api/users/usersRouter.js
+++ api/users/usersRouter.js
@@ -2,12 +2,15 @@ const router = require("express").Router();
 const User = require("./usersModel.js");
 const Classes = require("../classes/classesModel.js");
 const mw = require("../middleware/middleware.js");
+const { JWT_SECRET } = require('../secrets/index.js')
+const jwt = require('jsonwebtoken');
+const bycrypt = require('bcryptjs')
 
 router.get("/", (req, res, next) => {
   res.status(200).json({ users: "endpoint up" });
 });
 
-router.get("/:id", mw.checkUserId, (req, res, next) => {
+router.get("/:id", mw.checkUserId, mw.restricted, (req, res, next) => {
   try {
     const user = req.user;
     res.status(200).json(user);
@@ -17,7 +20,7 @@ router.get("/:id", mw.checkUserId, (req, res, next) => {
   }
 });
 
-router.put("/:id", async (req, res, next) => {
+router.put("/:id",mw.restricted, async (req, res, next) => {
   const { id } = req.params;
   const changes = req.body;
   try {
@@ -33,7 +36,7 @@ router.put("/:id", async (req, res, next) => {
   }
 });
 
-router.post("/:id/addClass", mw.checkUserId, async (req, res, next) => {
+router.post("/:id/addClass", mw.checkUserId, mw.restricted, async (req, res, next) => {
   const user = req.user;
   const addedClass = req.body;
   const role = req.decodedToken.role_name;
@@ -51,5 +54,69 @@ router.post("/:id/addClass", mw.checkUserId, async (req, res, next) => {
     next(err);
   }
 });
+
+router.post(
+  "/register",
+  mw.checkRegBody,
+  mw.checkEmailFree,
+  async (req, res, next) => {
+    const credentials = req.body;
+    try {
+      const hash = bcrypt.hashSync(credentials.password, 10);
+      credentials.password = hash;
+      const user = await Users.addUser(credentials);
+      res.status(201).json({ message: "User registered successfully!" });
+    } catch (err) {
+      err.message = "Server failed to register user";
+      next(err);
+    }
+  }
+);
+
+router.post(
+  "/login",
+  mw.checkLoginBody,
+  mw.checkEmailValid,
+  async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+      const user = await Users.findBy({ email }).first();
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: "Login successful!",
+          token,
+          user: {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            phone_number: user.phone_number,
+          },
+        });
+      } else {
+        res.status(401).json({ message: "invalid credentials!" });
+      }
+    } catch (err) {
+      err.message = "failed logging into server!";
+      next(err);
+    }
+  }
+);
+
+function generateToken(user) {
+  const payload = {
+    sub: user.id,
+    email: user.email,
+    role_name: user.role_id,
+  };
+
+  const options = {
+    expiresIn: "1h",
+  };
+
+  const secret = JWT_SECRET;
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
